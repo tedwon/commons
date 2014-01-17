58c9ec5c7b880957c9fa7e4cb308f621d75a1ae1
tedwon <iamtedwon@gmail.com>
2014-01-18 08:44:43 +0900

    Add test class

---
+++ src/test/java/com/realtimecep/commons/network/BindingTest.java
@@ -0,0 +1,19 @@
+package com.realtimecep.commons.network;
+
+import org.junit.Test;
+
+/**
+ * <p/>
+ *
+ * @author <a href=mailto:iamtedwon@gmail.com">Ted Won</a>
+ * @version 0.1.0
+ * @since 0.1.0
+ */
+public class BindingTest {
+
+  @Test
+  public void test() {
+
+  }
+
+}
